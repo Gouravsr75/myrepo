77ccb54c88784a3198d33301907910426cb885d4
Gouravsr75 <gourav7234@gmail.com>
2025-07-31 22:10:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+// const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
 
-io.on('connection', socket => {
-  console.log('User connected');
+// io.on('connection', socket => {
+//   console.log('User connected');
 
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+//   socket.on('chat message', msg => {
+//     io.emit('chat message', msg);
+//   });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
+//   socket.on('disconnect', () => {
+//     console.log('User disconnected');
+//   });
+// });
 
-http.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// http.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
